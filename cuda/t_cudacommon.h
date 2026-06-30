5aefad044bebc214d28b855a28f537b5439b05f5
SomeInternet <tfong@seas.upenn.edu>
2026-06-30 13:33:01 -0700

    CMake port

---
+++ cuda/t_cudacommon.h
@@ -0,0 +1,128 @@
+#pragma once
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+#include "vulkan/vulkan_core.h"
+
+#define GLM_FORCE_CUDA 
+#define GLM_FORCE_RADIANS 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "t_geometry.h"
+
+#define CUDA_CHECK(x)																																\
+	do {																																			\
+		cudaError_t err = x;																														\
+		if (err != cudaSuccess) {																													\
+			std::cerr << "CUDA error at file: " << __FILE__ << ", line: " << __LINE__ << ", error: " << cudaGetErrorString(err) << std::endl;	\
+			abort();																																\
+		}																																			\
+	} while (0)
+
+#define divup(a, b) ((a % b == 0) ? (a / b) : (a / b + 1))
+
+#define BLOCKWIDTH 16
+#define BLOCKHEIGHT 16
+
+class TsukiEngine; //Forward declaration
+
+struct TsukiLaunchDims {
+	dim3 gridDim;
+	dim3 blockDim;
+};
+
+struct u8vec4 {
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t a;
+};
+
+class TsukiCamera;
+struct TsukiCudaMesh;
+
+struct TsukiCudaCamera {
+	glm::vec3 origin;
+	glm::vec3 forward;
+	glm::vec3 right;
+	glm::vec3 up;
+	float fov;
+};
+
+struct TsukiCudaMaterialData {
+	int colorTextureIndex{ -1 };
+	int metallicRoughnessTextureIndex{ -1 };
+	int emissiveTextureIndex{ -1 };
+	int normalTextureIndex{ -1 };
+
+	glm::vec4 colorFac;
+	glm::vec4 metallicRoughnessFac;
+	glm::vec4 emissionFac;
+
+	uint8_t passPadding[16];
+
+	glm::vec4 padding[11]; //Padding to meet the 256 bytes
+};
+
+struct TsukiCudaMesh {
+	int numIndices{ 0 };
+	cudaExternalMemory_t indexBufferMemory;
+	uint32_t *d_indexBuffer;
+
+	int numVertices{ 0 };
+	cudaExternalMemory_t posMemory;
+	glm::vec4 *d_pos;
+
+	cudaExternalMemory_t normalMemory;
+	glm::vec4 *d_normal;
+
+	cudaExternalMemory_t colorMemory;
+	glm::vec4 *d_color;
+
+	cudaExternalMemory_t uvMemory;
+	glm::vec2 *d_uv;
+
+	int materialLookupBufferSize{ 0 };
+	cudaExternalMemory_t MaterialLookupBufferMemory;
+	int *d_materialLookupBuffer;
+};
+
+//Forward declarations
+struct TsukiCudaAccelerationStructures {
+	int tlasSize{ 0 };
+	TLASNode *d_tlas;
+
+	int numBVH{ 0 };
+	glm::mat4 *d_transforms;
+	glm::mat4 *d_invTransforms;
+	int *d_bvhSizes;
+	BVHNode **d_bvh;
+};
+
+struct TsukiCudaData { //TODO: This bottlenecks performance. I might wanna improve this later...
+	cudaExternalSemaphore_t _cudaSampleFinishedSemaphore;
+	cudaExternalSemaphore_t _cudaCopyFinishedSemaphore;
+
+	cudaExternalSemaphore_t _cudaASBuildFinishedSemaphore;
+
+	cudaExternalMemory_t imageBufferMemory;
+	void *imageBuffer{ nullptr };
+	int imageBufferSize{ 0 };
+
+	bool asBuilt{ false };
+	bool asDirty{ false };
+
+	bool resetImage{ false };
+
+	unsigned numSamples;
+
+	TsukiCudaCamera *d_camera{ nullptr };
+
+	void *d_MaterialBuffer{ nullptr };
+
+	int numMeshes;
+	TsukiCudaMesh *d_meshes{ nullptr };
+	TsukiCudaAccelerationStructures *d_accelerationStructures{ nullptr };
+};
---
+++ src/t_types.h
@@ -0,0 +1,208 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <span>
+#include <array>
+#include <functional>
+#include <deque>
+#include <iostream>
+
+#define NOMINMAX //Stop windows.h definitions of min and max
+#define VK_USE_PLATFORM_WIN32_KHR //Allows us to enable KHR external memory win32
+#include <windows.h>
+#include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+
+#include <vk_mem_alloc.h>
+
+#include "t_cudacommon.h"
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+//The do-while helps w/ expansion, and scopes the err
+//TODO: Either fix the fmt stuff, or just use std::cout or std::cerr
+#define VK_CHECK(x)																																\
+	do {																																		\
+		VkResult err = x;																														\
+		if (err) {																																\
+			std::cerr << "Vulkan error at file: " << __FILE__ << ", line: " << __LINE__ << ", error: " << string_VkResult(err) << std::endl;	\
+			abort();																															\
+		}																																		\
+	} while (0)
+
+#define PI 3.14159265358979f
+#define EPSILON .0001f
+
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+	std::optional<uint32_t> presentFamily;
+	//Note that transfer operations are implicitly supported by any queue family that supports graphics or compute operations
+
+	bool isComplete() {
+		return (graphicsFamily.has_value() && presentFamily.has_value());
+	}
+};
+
+struct SwapChainSupportDetails {
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+};
+
+struct DeletionQueue { //TODO: Optimize? (https://vkguide.dev/docs/new_chapter_2/vulkan_new_rendering/)
+	std::deque<std::function<void()>> deletors;
+
+	void push(std::function<void()> &&function) {
+		deletors.push_back(function);
+	}
+
+	void flush() {
+		for (auto it = deletors.rbegin(); it != deletors.rend(); ++it) {
+			(*it)();
+		}
+
+		deletors.clear();
+	}
+};
+
+struct AllocatedImage {
+	VkImage image;
+	VkImageView imageView;
+	VmaAllocation allocation;
+	VkExtent3D imageExtent;
+	VkFormat imageFormat;
+};
+
+struct AllocatedBuffer {
+	VkBuffer buffer; //Vulkan handle to the buffer
+	VmaAllocation allocation;
+	VmaAllocationInfo info;
+};
+
+constexpr uint32_t MAX_BINDLESS_TEXTURES = 4096;
+
+struct DeviceMeshBuffers {
+	AllocatedBuffer indexBuffer;
+	AllocatedBuffer posBuffer;
+	AllocatedBuffer normalBuffer;
+	AllocatedBuffer colorBuffer;
+	AllocatedBuffer uvBuffer;
+	AllocatedBuffer materialLookupBuffer;
+
+	VkDescriptorSet perMeshDescriptorSet;
+};
+
+enum ViewMode {
+	VIEW_SOLID, VIEW_COLOR, VIEW_NORMAL
+};
+
+struct DrawPushConstants {
+	glm::mat4 worldMatrix;
+	glm::mat4 inverseTranspose;
+	ViewMode viewMode;
+};
+
+//
+struct SceneData { //TODO: Update
+	glm::mat4 view;
+	glm::mat4 proj;
+	glm::mat4 viewproj;
+
+	glm::vec3 camPos;
+
+	float ambient;
+};
+
+//MATERIALS
+//===================================================================================================================
+enum class TsukiMaterialPass : uint8_t {
+	TSUKI_MATERIAL_OPAQUE, TSUKI_MATERIAL_TRANSPARENT, TSUKI_MATERIAL_OTHER
+};
+
+//Contains handles for the pipeline objects of a material
+struct TsukiMaterialPipeline {
+	VkPipeline pipeline;
+	VkPipelineLayout layout;
+};
+
+//Contains the material pipeline handles for selecting opaque/transparent paths
+struct TsukiMaterial {
+	TsukiMaterialPipeline *pipeline;
+	TsukiMaterialPass passType;
+};
+
+//OBJECTS AND RENDERABLES
+//===================================================================================================================
+struct TsukiDrawContext; //Forward declaration
+
+class TRenderable {
+	virtual void queueDraw(const glm::mat4 &matrix, TsukiDrawContext &context) = 0;
+};
+
+struct TNode : public TRenderable {
+	std::weak_ptr<TNode> parent; //A weak pointer is a smart pointer that doesn't claim ownership over the resource pointed to
+	//(i.e. it doesn't extend its lifespan)
+
+	std::vector<std::shared_ptr<TNode>> children; //Child nodes in the scene graph. Drawn recursively when the parent is drawn
+
+	glm::mat4 localTransform;
+	glm::mat4 worldTransform;
+
+	void updateTransform(const glm::mat4 &parentWorldTransform) {
+		worldTransform = parentWorldTransform * localTransform;
+		for (auto child : children) {
+			child->updateTransform(worldTransform);
+		}
+	}
+
+	virtual void queueDraw(const glm::mat4 &matrix, TsukiDrawContext &context) {
+		for (auto &child : children) {
+			child->queueDraw(matrix, context);
+		}
+	}
+};
+
+//REVIEW:
+//Command pools are thread-isolated pieces of memory corresponding to a queue family.
+//We allocated command buffers from them, which can be submitted to a queue in the queue family
+//of the command pool
+
+//Descriptor Set Layouts "promise" the Vulkan pipeline what kinds of resources
+//will be available at what stage of the pipeline. It groupts descriptor set layout bindings
+
+//Extensions fall into 2 categories: instance and device. Instance extensions enable Vulkan capabilities independent of the physical device
+
+//A Blit is a GPU-accelerated operation for pixel data transfer from 1 image to another
+
+//NOTES:
+//Spans are like views into arrays. They denote a contiguous region of memory hosting a certain number elements of a certain type
+
+//Descriptor set layouts are like a class. They tell us how a descriptor set will be laid out. A descriptor set is sort of like
+//an instance of that class. A descriptor is a pointer to GPU data accompanied by metadata. A pipeline layout tells the pipeline
+//what kinds of resources will be available and when
+
+//Blending occurs at the end of the graphics pipeline. It determines how our fragments interact with what is already in the attachment
+//Alpha and color blending are separated into 2 operations. We can blend multiplicatively, additively, etc. through VK_BLEND_OP_XXX
+//We can set the factor w/ VK_BLEND_FACTOR (e.g. VK_BLEND_FACTOR_ZERO, VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_SRC_ALPHA, VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA)
+
+//When a descriptor pool is created, it is given a maximum budget of sets and different kinds of descriptors. Sets can be allocated from the pool as
+//the programmer sees fit until the budget can't accommodate it anymore
+
+//std::vectors can be implicitly converted to std::spans
+
+//The advantage that weak pointers have over raw pointers is protection against dangling
+//To read or modify the data, you must temporarily promote the weak pointer to a shared pointer via .lock()
+//.lock() returns nullptr if the resource pointed to no longer exists
+//You can therefore use .lock() to check if the resource is still alive
+
+//glTF's (glb's, at least) store the base material colors in the material constants, in this implementation
+
+//Storage buffers can be bigger than uniform buffers
+
+//FOR GPU-side debugging: Put this into debugger:
+//C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.2\compute-sanitizer\compute-sanitizer.exe
